9783eb299b968e2afab1cb3cb3a1563feb982670
Ati1a <95629701+Ati1a@users.noreply.github.com>
2022-12-21 21:46:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 settings = {
-  'token': 'ваш токен'
-  'id': 'ваш id'
-  'file': 'полный путь к pdf файлу',
-  'file_name': 'название файла без .pdf'
+    'token': '5401062913:AAHPt5TqV0W-SSYMILDjB9_YZUcxn715nfk',
+    'id': '5401062913',
+    'file': 'C:/Users/ipetr/OneDrive/Desktop/Project/Приказ 6.18.1-05_160822-59.pdf',
+    'file_name': 'Приказ 6.18.1-05_160822-59',
+    'link': 'https://ba.hse.ru/news/admission/'
 }
